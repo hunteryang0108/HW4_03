6568fd018b36ac475f58f5e09ac638b17df53c10
Crazeah <183840676+Crazeah@users.noreply.github.com>
2025-04-20 17:56:12 +0800

    更新ta處理updateg以及圖片上傳異常

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,3 +1,8 @@
+<!-- Select2 CSS 和 JS -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 <x-layouts.app.sidebar :title="$title ?? null">
     <flux:main>
         {{ $slot }}
